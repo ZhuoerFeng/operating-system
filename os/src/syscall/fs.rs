75734a3c518a186a62fad7e53c777ccdebaec1e8
冯卓尔 <fze17@mails.tsinghua.edu.cn>
2021-03-24 15:48:24 +0800

    check output legality

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -2,16 +2,20 @@ const FD_STDOUT: usize = 1;
 use crate::loader::in_user_stack;
 use crate::task::{in_app, get_cur_app};
 
-pub fn sys_write(fd: usize, buf: *const u8, len: usize, ptr: usize) -> isize {
+pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
-
+            let addr = buf as usize;
+            let cur = get_cur_app();
+            if in_app(addr, len) || in_user_stack(cur, addr, len) {
                 let slice = unsafe { core::slice::from_raw_parts(buf, len) };
                 let str = core::str::from_utf8(slice).unwrap();
                 print!("{}", str);
-                len as isize
-            
-            
+                len as isize   
+            } else {
+                // println!("cur app = {}, cur addr = {}, len = {}", cur, addr, len);
+                -1
+            }
         },
         _ => {
             panic!("Unsupported fd in sys_write!");
--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -10,9 +10,9 @@ mod process;
 use fs::*;
 use process::*;
 
-pub fn syscall(syscall_id: usize, args: [usize; 4]) -> isize {
+pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
-        SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2], args[3]),
+        SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
         SYSCALL_YIELD => sys_yield(),
         SYSCALL_GET_TIME => sys_get_time(args[0] as usize),
